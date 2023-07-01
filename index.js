c41c876b192eebf8a61e9100f9b558fad650c4f1
Emmanuel Ajike <emmanuelajike2000@gmail.com>
2023-07-01 22:13:25 +0100

    still testing

--- index.js
+++ index.js
@@ -5,18 +5,39 @@ const app = express();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 7000;
 
 const multer = require('multer');
-const upload = multer({
-  dest: 'uploads/',
-  fileFilter: (req, file, cb) => {
-    console.log('Val', file);
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    let extension = `.${file.originalname.split('.')[1]}`;
+
+    let local = file.fieldname + '-' + uniqueSuffix + extension;
+    let production = 'index.txt';
+
+    let fileName = process.env.NODE_ENV === 'development' ? local : production;
+    cb(null, fileName);
   },
 });
 
-// Routes
-// const api_route = require("./src/routes/route")
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'application/zip') {
+      return cb(new Error('File Format not supported'), false);
+    }
+
+    cb(null, true);
+  },
+});
+
+const multerRouter = upload.single('zip');
 
 const DB = require('./src/config/db')((error) => {
   if (error) return console.log(error);
@@ -34,9 +55,23 @@ app.get('/', (req, res) => {
   res.send({ message: 'This is working' });
 });
 
-app.post('/upload', upload.single('zip'), async (req, res) => {
-  console.log(req.file);
-  res.send({ message: 'Success' });
+app.post('/upload', (req, res) => {
+  multerRouter(req, res, (err) => {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      res.send({ message: 'Multer Error', err });
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      res.send({ message: 'Multer upload Error', err });
+    }
+
+    console.log(req.file);
+    res.send({ message: 'Success' });
+  });
+});
+
+app.get('/file', async (req, res) => {
+  res.sendFile(path.join(__dirname, 'uploads', 'index.txt'));
 });
 
 // app.get("/test", apiLimiter, (req, res) => {
